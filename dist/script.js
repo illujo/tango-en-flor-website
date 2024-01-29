a1cc152731c939b365ab00449969eb14a6cd489f
illujo <luka.mar.94@gmail.com>
2024-01-29 22:56:28 +0100

    Svasta nesto

--- dist/script.js
+++ dist/script.js
@@ -1,24 +1,23 @@
- function handleScroll() {
-            // Get the current scroll position
-            var scrollPosition = window.scrollY;
-            var element = document.getElementById('header');
-            if(scrollPosition > 0){
-                element.classList.add('scroll-header');
-            } else {
-                element.classList.remove('scroll-header');
-            }
+function handleScroll() {
+  // Get the current scroll position
+  var scrollPosition = window.scrollY;
+  var element = document.getElementById("header");
+  if (scrollPosition > 0) {
+    element.classList.add("scroll-header");
+  } else {
+    element.classList.remove("scroll-header");
+  }
 
-            // Log or do something with the scroll position
-            console.log("Scroll Position: " + scrollPosition);
-        }
-        
+  // Log or do something with the scroll position
+  console.log("Scroll Position: " + scrollPosition);
+}
+
 function menuClicked() {
-    console.log('clicked');
-     var element = document.getElementById('mobile-menu');
-     var display = element.style.display;
-     if(display==="none"){
-        element.style.display = "block";    
-     } else {
-        element.style.display = "none";    
-     }
+  console.log("clicked");
+  var element = document.getElementById("mobile-menu");
+  if (!element.classList.includes("mobile-menu-open")) {
+    element.classList.add("scroll-header");
+  } else {
+    element.classList.remove("scroll-header");
+  }
 }
